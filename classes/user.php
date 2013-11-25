8dbb6158b1d6f36b2578f6c47ccd4ea7688ed1fe
Michael Borowsky <maborowsky@gmail.com>
2013-11-25 12:03:34 -0500

    Uses the organization class

--- classes/user.php
+++ classes/user.php
@@ -1,29 +1,26 @@
 <?php
 
-    /*
-     * TODO:
-     *    error checking
-     */
-
     class User {
 		    # The url of the api
 	        private $apiUrl = "https://forest-api.herokuapp.com/";
-
+	        
 			private $name     = "";
 			private $email    = "";
-			private $org      = "";
-			private $orgId    = "";
 			private $password = "";
 			private $admin    = False;
+			
+			# An organization object
+			private $organization;
 
-			public function __construct($name, $email, $org,
-			                              $password, $admin=False) {
+			public function __construct($name, $email, $orgName,
+			                            $password, $admin=False) {
 			    $this->name     = $name;
 			    $this->email    = $email;
-			    $this->org      = $org;
-			    $this->orgId    = $this->getOrgId($org);
+			    $this->orgName  = $org;
 			    $this->password = $password;
 			    $this->admin    = $admin;
+			    
+			    $organization = new Organization($orgName);
 			}
 
 			# Gets the parameters which can be passed to the api
@@ -31,7 +28,7 @@
 			public function getParams() {
 			    $url = "";
 
-			    $url .= "organizationId=" . $this->orgId;
+			    $url .= "organizationId=" . $this->organization->getId;
 			    $url .= "&name="     . $this->name;
 			    $url .= "&email="    . $this->email;
 			    $url .= "&password=" . $this->password;
@@ -41,24 +38,6 @@
 			    $url .= "&admin=" . $adminString;
 
 			    return $url;
-			}
-
-			# Gets the ID for a given organization
-			private function getOrgId($orgName) {
-			    # Get the list of organizations from the api
-                $url = "https://forest-api.herokuapp.com/organizationList";
-                $json = file_get_contents($url);
-
-	            $organizations = json_decode($json); # Decode the json
-	            $orgs = $organizations->organizations;
-
-                # Search for the organization
-                foreach ($orgs as $org) {
-                    if ($org->name == $orgName) # If the organization is found
-	                    return $org->id; # Return the id
-	            }
-
-	            return null;# If the organization isn't found, return null
 			}
 
 
