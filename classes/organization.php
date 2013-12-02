0711d8df8095fd8ed94e7b5f0fd530682e1158b2
Michael Borowsky <maborowsky@gmail.com>
2013-12-02 16:51:47 -0500

    Fix indentation

--- classes/organization.php
+++ classes/organization.php
@@ -1,36 +1,36 @@
 <?php
-    class Organization {
-			private $name     = "";
-			private $orgId    = "";
+class Organization {
+    private $name     = "";
+    private $orgId    = "";
 
-			public function __construct($name) {
-			    $this->name     = $name;
-			    $this->orgId    = $this->getOrgId($name);
-			}
+    public function __construct($name) {
+        $this->name     = $name;
+        $this->orgId    = $this->getOrgId($name);
+    }
 
-			# Gets the ID for a given organization
-			public static function getOrgId($orgName) {
-			    # Get the list of organizations from the api
-                $url = "https://forest-api.herokuapp.com/organizationList";
-                $json = file_get_contents($url);
+    # Gets the ID for a given organization
+    public static function getOrgId($orgName) {
+        # Get the list of organizations from the api
+        $url = "https://forest-api.herokuapp.com/organizationList";
+        $json = file_get_contents($url);
 
-	            $organizations = json_decode($json); # Decode the json
-	            $orgs = $organizations->organizations;
+        $organizations = json_decode($json); # Decode the json
+            $orgs = $organizations->organizations;
 
-                # Search for the organization
-                foreach ($orgs as $org) {
-                    if ($org->name == $orgName) # If the organization is found
-	                    return $org->id; # Return the id
-	            }
+        # Search for the organization
+        foreach ($orgs as $org) {
+            if ($org->name == $orgName) # If the organization is found
+                return $org->id; # Return the id
+        }
 
-	            return null; # If the organization isn't found, return null
-			}
-			
-			# Gets the ID for the organization
-			public function getId() {
-				return $this->orgId;
-			}
+        return null; # If the organization isn't found, return null
+    }
+
+    # Gets the ID for the organization
+    public function getId() {
+        return $this->orgId;
+    }
 
 
-		} # End class
+} # End class
 ?>
--- classes/user.php
+++ classes/user.php
@@ -1,45 +1,45 @@
 <?php
 
-    class User {
-		    # The url of the api
-	        private $apiUrl = "https://forest-api.herokuapp.com/";
-	        
-			private $name     = "";
-			private $email    = "";
-			private $password = "";
-			private $admin    = False;
-			
-			# An organization object
-			private $organization;
+class User {
+    # The url of the api
+    private $apiUrl = "https://forest-api.herokuapp.com/";
 
-			public function __construct($name, $email, $orgName,
-			                            $password, $admin=False) {
-			    $this->name     = $name;
-			    $this->email    = $email;
-			    $this->orgName  = $orgName;
-			    $this->password = $password;
-			    $this->admin    = $admin;
-			    
-			    $this->organization = new Organization($orgName);
-			}
+    private $name     = "";
+    private $email    = "";
+    private $password = "";
+    private $admin    = False;
 
-			# Gets the parameters which can be passed to the api
-            # Returns a string which can be appended to the end of a url
-			public function getParams() {
-			    $url = "";
+    # An organization object
+    private $organization;
 
-			    $url .= "organizationId=" . $this->organization->getId();
-			    $url .= "&name="     . $this->name;
-			    $url .= "&email="    . $this->email;
-			    $url .= "&password=" . $this->password;
+    public function __construct($name, $email, $orgName,
+            $password, $admin=False) {
+        $this->name     = $name;
+        $this->email    = $email;
+        $this->orgName  = $orgName;
+        $this->password = $password;
+        $this->admin    = $admin;
 
-			    # Get the string representation of admin
-			    $adminString = $this->admin ? "true" : "false";
-			    $url .= "&admin=" . $adminString;
+        $this->organization = new Organization($orgName);
+    }
 
-			    return $url;
-			}
+    # Gets the parameters which can be passed to the api
+    # Returns a string which can be appended to the end of a url
+    public function getParams() {
+        $url = "";
+
+        $url .= "organizationId=" . $this->organization->getId();
+        $url .= "&name="     . $this->name;
+        $url .= "&email="    . $this->email;
+        $url .= "&password=" . $this->password;
+
+        # Get the string representation of admin
+        $adminString = $this->admin ? "true" : "false";
+        $url .= "&admin=" . $adminString;
+
+        return $url;
+    }
 
 
-		} # End class
+} # End class
 ?>
